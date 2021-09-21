67db9a176207cb32ccefd6c582a4c465bfe98f77
nmandal <nicholas.mandal@gmail.com>
2021-09-21 14:07:54 -0500

    pretty

--- pages/api/revue.js
+++ pages/api/revue.js
@@ -1,15 +1,10 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default async function handler(req, res) {
-    // 1. Get the email from the payload and
-    // validate if it is empty.
     const { email } = req.body;
     if (!email) {
         return res.status(400).json({error: 'Email is required.'});
     }
-  
-    // 2. Use the Revue API Key and create a subscriber using
-    // the email we pass to the API. Please note, we pass the
-    // API Key in the 'Authorization' header.
+    
     try {
         const API_KEY = process.env.REVUE_API_KEY;
         const revueRoute = `${process.env.REVUE_API_URL}subscribers`
