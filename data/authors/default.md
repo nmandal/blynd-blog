3a8e5a6abb16868b2b308d104cc17c0377c06115
nmandal <nicholas.mandal@gmail.com>
2021-09-21 13:47:46 -0500

    first commit

---
+++ data/authors/default.md
@@ -0,0 +1,12 @@
+---
+name: Nick Mandal
+avatar: /static/images/avatar.png
+occupation: Founder, Blynd
+company: Blynd, Inc.
+email: nick@eatblynd.com
+twitter: https://twitter.com/nick_mandal
+linkedin: https://www.linkedin.com/in/nicholasmandal
+github: https://github.com/nmandal
+---
+
+Nick Mandal is the founder of Blynd.
---
+++ data/projectsData.js
@@ -0,0 +1,16 @@
+const projectsData = [
+  {
+    title: 'Blynd App',
+    description: `An online food ordering platform that takes the decision fatigue out of deciding what to order.`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://eatblynd.com',
+  },
+  {
+    title: 'MenYou',
+    description: `Build and share custom restaurant guides.`,
+    imgSrc: '/static/images/time-machine.jpg',
+    href: 'https://menyou.eatblynd.com',
+  },
+]
+
+export default projectsData
